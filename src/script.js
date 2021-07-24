7e9ba3d001e16727bea7314b656ac567da82319d
Rahul Ajmani <rahulajmani910@gmail.com>
2021-07-24 18:46:07 +0530

    adding position

--- src/script.js
+++ src/script.js
@@ -5,22 +5,42 @@ import * as THREE from 'three'
 const scene = new THREE.Scene()
 
 // Red Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale
+// mesh.scale.x = 0.2
+// mesh.scale.y = 0.2
+// mesh.scale.x = 0.2
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 // Sizes
 const sizes = {
-    width: 800,
-    height: 800
+    width: 600,
+    height: 600
 }
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
-
 
 // Renderer
 const canvas = document.querySelector('.webgl');
