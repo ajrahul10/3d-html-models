ec82c67e8d28748cef7defb994b472b9c33ba938
Rahul Ajmani <rahulajmani910@gmail.com>
2021-07-24 18:54:37 +0530

    adding group

--- src/script.js
+++ src/script.js
@@ -4,28 +4,32 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
-// Red Cube
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// Object
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000}) 
+)
+group.add(cube1);
 
-// Scale
-// mesh.scale.x = 0.2
-// mesh.scale.y = 0.2
-// mesh.scale.x = 0.2
-mesh.scale.set(2, 0.5, 0.5)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00}) 
+)
+cube2.position.set(-1.5, 0, 0);
+group.add(cube2);
 
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff}) 
+)
+cube3.position.set(1.5, 0, 0);
+group.add(cube3);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
@@ -33,8 +37,8 @@ scene.add(axesHelper);
 
 // Sizes
 const sizes = {
-    width: 600,
-    height: 600
+    width: 800,
+    height: 800
 }
 
 // Camera
